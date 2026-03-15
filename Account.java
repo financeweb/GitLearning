2254a2b9f0b3091c10b70a228d83c91c3de78421
thenaruvi.jayanthi@gmail.com <thenaruvi.jayanthi@gmail.com>
2026-03-15 14:55:30 -0400

    Initial commit V1.0

---
+++ Account.java
@@ -0,0 +1,8 @@
+package GITCommands;
+
+public class Account {
+    public static void main(String[] arg) {
+        Payment p = new Payment();
+        System.out.println("Account.java v1.0");
+    }
+}
