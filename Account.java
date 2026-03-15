3d64245a784c5fed121bb518eede6a51c3e23d5b
financeweb <91275891+financeweb@users.noreply.github.com>
2026-03-15 14:58:57 -0400

    Add edit message to Account.java output

--- Account.java
+++ Account.java
@@ -4,5 +4,6 @@ public class Account {
     public static void main(String[] arg) {
         Payment p = new Payment();
         System.out.println("Account.java v1.0");
+        System.out.println("Account.java v1.0 edited in github");
     }
 }
